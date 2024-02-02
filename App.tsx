1cd79bf9be15c4e06404402be926e7f67ce5ef32
HaidarShah208 <alhi7896542@gmail.com>
2024-02-03 01:33:51 +0500

    ss

--- App.tsx
+++ App.tsx
@@ -2,8 +2,9 @@ import React, { useEffect } from 'react'
 import Navigator from './src/navigation/stackNavigation/Navigator'
 import Toast from 'react-native-toast-message';
 import SplashScreen from 'react-native-splash-screen';
-
-
+import TabNavigator from './src/navigation/tabNavigation/Navigator';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import DrawerNavigator from './src/navigation/drawerNavigation/DrawerNavigator';
 export default function App() {
   useEffect(() => {
 
@@ -16,9 +17,11 @@ export default function App() {
     return () => clearTimeout(timeoutId);
   }, []);
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
   <Navigator/>
+ {/* <TabNavigator/> */}
+ {/* <DrawerNavigator/> */}
   <Toast />
-    </>
+    </GestureHandlerRootView>
   )
 }
