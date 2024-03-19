2e0556e38b6646c11abd7139b8df613559c22582
HaidarShah208 <alhi7896542@gmail.com>
2024-03-19 22:53:14 +0500

    fix issues

---
+++ src/screens/donateScreen/DonateScreen.tsx
@@ -0,0 +1,224 @@
+import {
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
+import React, {useState} from 'react';
+import {IMAGES} from '../../constants/assessts/AllAssets';
+import Button from '../../components/button/Button';
+import {styles} from './DonateScreenStyle';
+import {StackNavigationProp} from '@react-navigation/stack';
+import {RootStackParamsDetailsList} from '../../navigation/detailNavigation/DetailNavigation';
+import {AppThunk} from '../../store/store';
+import useDonationScreen from './useDonationScreen';
+
+interface donateScreenProps {
+  navigation: StackNavigationProp<RootStackParamsDetailsList, 'Add_Pet'>;
+}
+
+export default function DonateScreen({navigation}: donateScreenProps) {
+  const {
+    handleChange,
+    handlePressGender,
+    handlePressType,
+    handlePressVaccinated,
+    uploadImage,
+    isGenderClick,
+    isPetTypeClicked,
+    isVaccinatedClick,
+    allData,
+    setIsGenderClick,
+    setIsPetTypeClicked,
+    setIsVaccinatedClick,
+    state,
+    selectedValues,
+    onSearch,
+    loading,
+    vaccinated,
+    gender,
+    selectFile,
+  } = useDonationScreen();
+
+  return (
+    <ScrollView>
+      <TouchableOpacity
+        onPress={() => {
+          navigation.goBack();
+        }}>
+        <IMAGES.BACK width={25} height={25} style={styles.iMg} />
+      </TouchableOpacity>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Pet Type</Text>
+        <TouchableOpacity style={styles.dorpdown} onPress={handlePressType}>
+          <Text style={{color: '#101C1D'}}>{selectedValues.petType}</Text>
+          {isPetTypeClicked ? <IMAGES.upArrow /> : <IMAGES.downArrow />}
+        </TouchableOpacity>
+        {isPetTypeClicked ? (
+          <View style={styles.dropdownArea}>
+            <TextInput
+              placeholder="search"
+              style={styles.searchInput}
+              onChangeText={text => {
+                onSearch(text);
+              }}
+            />
+            <FlatList
+              scrollEnabled={false}
+              data={allData}
+              renderItem={({item, index}) => {
+                return (
+                  <TouchableOpacity
+                    style={styles.Item}
+                    onPress={() => {
+                      handleChange('petType', item.dog);
+                      onSearch('');
+                      setIsPetTypeClicked(false);
+                    }}>
+                    <Text style={styles.ItemText}>{item.dog}</Text>
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </View>
+        ) : null}
+        <Text style={styles.mail}>Pet Breed</Text>
+        <TextInput
+          style={styles.input}
+          value={state.petBreed}
+          onChangeText={text => handleChange('petBreed', text)}
+        />
+        <Text style={styles.mail}>Pet Age</Text>
+        <TextInput
+          style={styles.input}
+          value={state.petAge}
+          keyboardType="numeric"
+          onChangeText={text => handleChange('petAge', text)}
+        />
+        <Text style={styles.mail}>Amount</Text>
+        <TextInput
+          style={styles.input}
+          value={`$ ${state.amount}`}
+          onChangeText={text =>
+            handleChange('amount', text.replace('$', '').trim())
+          }
+          keyboardType="numeric"
+        />
+
+        <Text style={styles.heading}>Vaccinated</Text>
+        <TouchableOpacity
+          style={styles.dorpdown}
+          onPress={handlePressVaccinated}>
+          <Text style={{color: '#101C1D'}}>{selectedValues.vaccinated}</Text>
+          {isVaccinatedClick ? <IMAGES.upArrow /> : <IMAGES.downArrow />}
+        </TouchableOpacity>
+        {isVaccinatedClick ? (
+          <View style={styles.dropdownArea}>
+            <TextInput
+              placeholder="search"
+              style={styles.searchInput}
+              onChangeText={text => {
+                onSearch(text);
+              }}
+            />
+            <FlatList
+              scrollEnabled={false}
+              data={vaccinated}
+              renderItem={({item, index}) => {
+                return (
+                  <TouchableOpacity
+                    style={styles.Item}
+                    onPress={() => {
+                      handleChange('vaccinated', item.isVaccinated);
+                      onSearch('');
+                      setIsVaccinatedClick(false);
+                    }}>
+                    <Text style={styles.ItemText}>{item.isVaccinated}</Text>
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </View>
+        ) : null}
+        <Text style={styles.heading}>Gender</Text>
+        <TouchableOpacity style={styles.dorpdown} onPress={handlePressGender}>
+          <Text style={{color: '#101C1D'}}>{selectedValues.gender}</Text>
+          {isGenderClick ? <IMAGES.upArrow /> : <IMAGES.downArrow />}
+        </TouchableOpacity>
+        {isGenderClick ? (
+          <View style={styles.dropdownArea}>
+            <TextInput
+              placeholder="search"
+              style={styles.searchInput}
+              onChangeText={text => {
+                onSearch(text);
+              }}
+            />
+            <FlatList
+              scrollEnabled={false}
+              data={gender}
+              renderItem={({item, index}) => {
+                return (
+                  <TouchableOpacity
+                    style={styles.Item}
+                    onPress={() => {
+                      handleChange('gender', item.gender);
+                      onSearch('');
+                      setIsGenderClick(false);
+                    }}>
+                    <Text style={styles.ItemText}>{item.gender}</Text>
+                  </TouchableOpacity>
+                );
+              }}
+            />
+          </View>
+        ) : null}
+        <Text style={styles.mail}>Weight</Text>
+        <TextInput
+          style={styles.input}
+          value={`KG ${state.petWeight}`}
+          onChangeText={text =>
+            handleChange('petWeight', text.replace('KG', '').trim())
+          }
+          keyboardType="numeric"
+        />
+        <Text style={styles.mail}>Location</Text>
+        <TextInput
+          style={styles.input}
+          value={state.petLocation}
+          onChangeText={text => handleChange('petLocation', text)}
+        />
+        <Text style={styles.mail}>Description</Text>
+        <TextInput
+          style={styles.input}
+          value={state.description}
+          onChangeText={text => handleChange('description', text)}
+        />
+        <Text style={styles.heading}>Image</Text>
+        <TouchableOpacity style={styles.img} onPress={selectFile}>
+          <IMAGES.ImagePicker />
+        </TouchableOpacity>
+        <View style={styles.btns}>
+          <Button
+            title={
+              loading ? (
+                <ActivityIndicator size="large" color="white" />
+              ) : (
+                'Donate'
+              )
+            }
+            buttonStyle={styles.btns}
+            onPress={() => uploadImage()}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+function dispatch(arg0: AppThunk) {
+  throw new Error('Function not implemented.');
+}
---
+++ src/screens/donationRequests/DonationRequestsStyle.ts
@@ -0,0 +1,82 @@
+import {StyleSheet} from 'react-native';
+import {FONTS} from '../../constants/fonts/AllFonts';
+
+export const requesStyles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  card: {
+    justifyContent: 'center',
+    marginBottom: 10,
+    width: 330,
+    height: 194,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+    shadowColor: '#000',
+    elevation: 2,
+  },
+  heading: {
+    marginTop: 35,
+    paddingStart: 35,
+    color: '#101C1D',
+    fontFamily: FONTS.SemiBold,
+    fontSize: 24,
+    marginBottom: 20,
+    alignSelf: 'flex-start',
+  },
+  img: {
+    marginStart: 20,
+    marginRight: 15,
+    marginTop: 15,
+    width: 72,
+    height: 72,
+    borderRadius: 50,
+    overflow:'hidden'
+  },
+  InfoContiaenr: {
+    marginTop: 10,
+  },
+  furthurInfo: {
+    fontFamily: FONTS.SemiBold,
+    fontSize: 18,
+    color: '#101C1D',
+  },
+  smallText: {
+    paddingTop: 5,
+    fontFamily: FONTS.REGULER,
+    fontSize: 14,
+    color: '#101C1D',
+  },
+  small: {
+    fontFamily: FONTS.REGULER,
+    fontSize: 14,
+    color: '#101C1D',
+  },
+  button: {
+    marginTop: 10,
+    alignSelf: 'center',
+    backgroundColor: '#101C1D',
+    textAlign: 'center',
+    color: 'white',
+    fontFamily: FONTS.SemiBold,
+    height: 36,
+    borderRadius: 20,
+    width: 225,
+    paddingTop: 5,
+  },
+  loader:{
+    flex:1,justifyContent:'center',alignItems:'center'
+  },
+  flex:{
+    display:'flex',
+    flexDirection:'row'
+  },
+  dash:{
+    marginHorizontal:5,fontWeight:'900',
+    paddingTop:5
+  },
+
+
+});
